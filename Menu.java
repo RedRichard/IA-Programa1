e33d51b5a50a53474abbde48fa4e1d701db5584d
RedRichard <gomric@hotmail.com>
2018-08-15 16:48:33 -0500

    Cambios clase Menu
    Básicamente agregué un par de funciones para facilitar el trabajo en el switch de los casos.

---
+++ Menu.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+//Clase encargada de desplegar el menu
+public class Menu{
+    private String [] vector;
+    public Menu()
+    {
+        String opcion = JOptionPane.showInputDialog(null,"Escoja una de las siguientes opciones\n1.- Suma\n2.- Resta\n3.- Producto por un escalar\n4.- Norma\n5.-Angulo entre dos vectores" );
+        
+        switch (opcion){
+            case "4":   
+                SubmenuVector();
+                MostrarMensaje("La norma es: " + OperacionesVector.norma(vector));
+                break;
+            default:
+                //En caso de que la opcion no sea válida se ejecuta el menu otravez
+                Menu menu = new Menu();
+        }
+    }    
+
+    public void SubmenuVector(){
+        vector = JOptionPane.showInputDialog(null,"Escriba el vector separado por comas\nEjem.  10,2,7,40,56" ).split(",");
+    }
+
+    public void MostrarMensaje(String mensaje){
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+}
