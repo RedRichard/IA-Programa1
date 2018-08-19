de5e490d7b72cfb35a1d38f48538df4d41418055
Angel Hernandez <angel96esti@comunidad.unam.mx>
2018-08-19 15:05:59 -0500

    Manejo de excepciones

--- Menu.java
+++ Menu.java
@@ -9,78 +9,92 @@ import java.util.Arrays;
 
 //Clase encargada de desplegar el menu
 public class Menu{
-    private String [] vector1, vector2;
-    private int escalar;
-    public Menu()
-    {
-        String opcion = JOptionPane.showInputDialog(null,"Escoja una de las siguientes opciones\n1.- Suma\n2.- Resta\n3.- Producto por un escalar\n4.- Norma\n5.-Angulo entre dos vectores" );
+	private String [] vector1, vector2;
+	private float escalar;
+	public Menu() {
+		String opcion = JOptionPane.showInputDialog(null,"Escoja una de las siguientes opciones\n1.- Suma\n2.- Resta\n3.- Producto por un escalar\n4.- Norma\n5.-Angulo entre dos vectores" );
+		
+		switch(opcion) {
+			case "1":
+				subMenuSumaResta();
+				if(vector1.length != vector2.length) {
+					JOptionPane.showMessageDialog(null,"La longitud de los vectores es diferente"
+						,"error",JOptionPane.ERROR_MESSAGE);
+				} else {
+					try {
+						MostrarMensaje("el nuevo vector es: " + Arrays.toString(OperacionesVector.sumaDeVectores(vector1, vector2)));
+					} catch (NumberFormatException e) {
+						JOptionPane.showMessageDialog(null,"Uno de los elementos del vector no es un numero"
+						,"error",JOptionPane.ERROR_MESSAGE);
+					}
+				}
+				break;
+			case "2":
+				subMenuSumaResta();
+				if(vector1.length != vector2.length) {
+					JOptionPane.showMessageDialog(null,"La longitud de los vectores es diferente"
+						,"error",JOptionPane.ERROR_MESSAGE);
+				} else {
+					try {
+						MostrarMensaje("el nuevo vector es: " + Arrays.toString(OperacionesVector.restaDeVectores(vector1, vector2)));
+					} catch (NumberFormatException e) {
+						JOptionPane.showMessageDialog(null,"Uno de los elementos del vetor no es un numero"
+						,"error",JOptionPane.ERROR_MESSAGE);
+					}
+				}
+				break;
+			case "3":
+				SubmenuVector();
+				try {
+					SubmenuEscalar();
+					MostrarMensaje("El producto del vector por el escalar es: " + Arrays.toString(OperacionesVector.productoEscalar(vector1, escalar)));
+				} catch (NumberFormatException e) {
+					MostrarMensaje("El escalar o uno de los elementos del vector no es un numero.");
+				}
+				break;
+			case "4":
+				SubmenuVector();
+				try {
+					MostrarMensaje("La norma es: " + OperacionesVector.norma(vector1));
+				} catch (NumberFormatException e) {
+					JOptionPane.showMessageDialog(null,"Uno de los elementos del vector no es un numero"
+						,"error",JOptionPane.ERROR_MESSAGE);
+				}
+				break;
+			default:
+				//En caso de que la opcion no sea válida se ejecuta el menu otravez
+				//Menu menu = new Menu();
+		}
+	}
 
-        switch (opcion){
-            case "1":
-                subMenuSumaResta();
-                if(vector1.length != vector2.length){
-                    JOptionPane.showMessageDialog(null,"La longitud de los vectores es diferente"
-                    ,"error",JOptionPane.ERROR_MESSAGE);
-                }else {
-                    MostrarMensaje("el nuevo vector es: " +
-                    Arrays.toString(OperacionesVector.sumaDeVectores(vector1, vector2)));
-                }
-            break;
-            case "2":
-                subMenuSumaResta();
-                if(vector1.length != vector2.length){
-                    JOptionPane.showMessageDialog(null,"La longitud de los vectores es diferente"
-                    ,"error",JOptionPane.ERROR_MESSAGE);
-                }else {
-                    MostrarMensaje("el nuevo vector es: " +
-                    Arrays.toString(OperacionesVector.restaDeVectores(vector1, vector2)));
-                }
-            break;
-            case "3":
-                SubmenuVector();
-                SubmenuEscalar();
-                MostrarMensaje("El producto de por el escalar es: " + Arrays.toString(OperacionesVector.productoEscalar(vector1, escalar)));
-            break;
-            case "4":
-                SubmenuVector();
-                MostrarMensaje("La norma es: " + OperacionesVector.norma(vector1));
-                break;
-            default:
-                //En caso de que la opcion no sea válida se ejecuta el menu otravez
-                Menu menu = new Menu();
-            break;
-        }
-    }
+	public void subMenuSumaResta() {
+		JTextField xField = new JTextField(100);
+		JTextField yField = new JTextField(100);
 
-    public void subMenuSumaResta(){
-        JTextField xField = new JTextField(100);
-        JTextField yField = new JTextField(100);
+		JPanel myPanel = new JPanel();
+		myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
+		myPanel.add(new JLabel("dame el vector x separado por comas"));
+		myPanel.add(xField);
+		myPanel.add(new JLabel("dame el vector y separado por comas"));
+		myPanel.add(yField);
+		int result = JOptionPane.showConfirmDialog(null, myPanel, "Please Enter X and Y Values",
+			JOptionPane.OK_CANCEL_OPTION);
+		if(result == JOptionPane.OK_OPTION) {
+			vector1 = xField.getText().split(",");
+			vector2 = yField.getText().split(",");
+			}
+	}
 
-        JPanel myPanel = new JPanel();
-        myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
-        myPanel.add(new JLabel("dame el vector x separado por comas"));
-        myPanel.add(xField);
-        myPanel.add(new JLabel("dame el vector y separado por comas"));
-        myPanel.add(yField);
-        int result = JOptionPane.showConfirmDialog(null, myPanel, "Please Enter X and Y Values",
-                JOptionPane.OK_CANCEL_OPTION);
-        if (result == JOptionPane.OK_OPTION) {
-            System.out.println("x value: " + xField.getText());
-            System.out.println("y value: " + yField.getText());
-            vector1 = xField.getText().split(",");
-            vector2 = yField.getText().split(",");
-        }
-    }
+	public void SubmenuVector(){
+		vector1 = JOptionPane.showInputDialog(null,"Escriba el vector separado por comas\nEjem.  10,2,7,40,56" ).split(",");
+	}
 
-    public void SubmenuVector(){
-        vector1 = JOptionPane.showInputDialog(null,"Escriba el vector separado por comas\nEjem.  10,2,7,40,56" ).split(",");
-    }
+	public void SubmenuEscalar(){
+		escalar = Float.parseFloat(JOptionPane.showInputDialog(null, "Escriba el escalar: "));
+	}
 
-    public void SubmenuEscalar(){
-        escalar = Integer.parseInt(JOptionPane.showInputDialog(null, "Escriba el escalar: "));
-    }
-
-    public void MostrarMensaje(String mensaje){
-        JOptionPane.showMessageDialog(null, mensaje);
-    }
+	public void MostrarMensaje(String mensaje){
+		JOptionPane.showMessageDialog(null, mensaje);
+		Menu menu = new Menu();
+	}
 }
--- OperacionesVector.java
+++ OperacionesVector.java
@@ -1,44 +1,42 @@
 public class OperacionesVector{
-    private String [] auxVector;
-    public static float norma(String [] vector){
-        float suma = 0, indice;
-        for (String a: vector)
-        {
-            indice = Float.parseFloat(a);
-            indice = (float) Math.pow(indice,2);
-            suma += indice;
-        }
-        return (float) Math.pow(suma, 0.5);
-    }
+		private String [] auxVector;
+		public static float norma(String [] vector){
+				float suma = 0, indice;
+				for (String a: vector) {
+						indice = Float.parseFloat(a);
+						indice = (float) Math.pow(indice,2);
+						suma += indice;
+				}
+				return (float) Math.pow(suma, 0.5);
+		}
 
-    public static String [] productoEscalar(String [] vector, int escalar){
-        for (int i=0; i<vector.length; i++) {
-            vector[i] = String.valueOf(Float.parseFloat(vector[i]) * escalar);
-        }
-        // System.out.println(Arrays.toString(vector));
-        return vector;
-    }
+		public static String [] productoEscalar(String [] vector, float escalar){
+				for (int i=0; i<vector.length; i++) {
+						vector[i] = String.valueOf(Float.parseFloat(vector[i]) * escalar);
+				}
+				return vector;
+		}
 
-    public static String [] restaDeVectores(String[] vector1,String[] vector2){
-        int length = vector1.length;
-        String[] vector3 = new String[length];
-        float[] resta = new float[length];
-        for (int i = 0; i < length; i++) {
-            resta[i] = Float.parseFloat(vector1[i]) - Float.parseFloat(vector2[i]);
-            vector3[i] = String.valueOf(resta[i]);
-        }
-        return vector3;
-    }
+		public static String [] restaDeVectores(String[] vector1,String[] vector2){
+				int length = vector1.length;
+				String[] vector3 = new String[length];
+				float[] resta = new float[length];
+				for (int i = 0; i < length; i++) {
+						resta[i] = Float.parseFloat(vector1[i]) - Float.parseFloat(vector2[i]);
+						vector3[i] = String.valueOf(resta[i]);
+				}
+				return vector3;
+		}
 
-    public static String [] sumaDeVectores(String[] vector1,String[] vector2){
-        int length = vector1.length;
-        String[] vector3 = new String[length];
-        float[] resta = new float[length];
-        for (int i = 0; i < length; i++) {
-            resta[i] = Float.parseFloat(vector1[i]) + Float.parseFloat(vector2[i]);
-            vector3[i] = String.valueOf(resta[i]);
-        }
-        return vector3;
-    }
+		public static String [] sumaDeVectores(String[] vector1,String[] vector2){
+				int length = vector1.length;
+				String[] vector3 = new String[length];
+				float[] resta = new float[length];
+				for (int i = 0; i < length; i++) {
+						resta[i] = Float.parseFloat(vector1[i]) + Float.parseFloat(vector2[i]);
+						vector3[i] = String.valueOf(resta[i]);
+				}
+				return vector3;
+		}
 
 }
